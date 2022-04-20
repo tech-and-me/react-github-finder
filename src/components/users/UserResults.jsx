c5be14e131920f3488e072042990d87b7a9376d5
Pary <81phary@gmail.com>
2022-04-20 22:11:18 +1000

    build user profile page

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import {useEffect,useState} from 'react'
 import Spinner from '../layouts/Spinner';
+import UserItem from './UserItem';
 
 
 const UserResults = () => {
@@ -12,12 +13,27 @@ const UserResults = () => {
     },[])
 
     const fetchUsers = async () => {
+
+        //*****Method 1: Using global variables store in .env
         const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
             headers:{
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
-        })     
+        })  
+
+        //*****Method 2: without using global variable
+        // const response = await fetch("https://api.github.com/users",{
+        //     headers:{
+        //         Authorization: "token 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"
+        //     }
+        // })     
+        
+
+        //*****Method 3: Without using global variable and without using token
+        // const response = await fetch("https://api.github.com/users")   
+
         const data = await response.json();
+        console.log(data);
         setUsers(data);
         setLoading(false);
     }
@@ -27,7 +43,7 @@ const UserResults = () => {
         return (
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                 {users.map((user)=>(
-                    <h3>{user.login}</h3>
+                    <UserItem key={user.id} user={user}/>
                 ))}
             </div>
           )
